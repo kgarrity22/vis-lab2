28c8eba5404338c28ab6fe20118885dd91d31576
kgarrity22 <garrityk@bc.edu>
2020-09-15 15:11:28 -0400

    loaded data into attractions

---
+++ script.js
@@ -0,0 +1,38 @@
+
+ 
+
+let attractions;
+fetch('attractions.json')
+  .then(response => response.json())
+  .then(data => {
+		attractions = data;
+		//console.log('attractions: ', attractions);
+	});
+
+
+
+
+
+
+// /* jsx
+
+// function filterData(category) {
+
+// 	/* **************************************************
+// 	 *
+// 	 * TODO: filter attractions by the selected category
+// 	 * TODO: filter top 5 attractions
+// 	 *
+// 	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
+// 	 *
+// 	 * renderBarChart(data)
+// 	 *
+// 	 * - 'data' must be an array of JSON objects
+// 	 * - the max. length of 'data' is 5
+// 	 *
+// 	 * **************************************************/
+
+// }
+
+// // TODO: Define an event listener for the dropdown menu
+// //       Call filterData with the selected category
